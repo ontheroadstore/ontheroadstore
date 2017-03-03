2c74451ff92fd62746d32a7862d2c619ace368a4
PsychoKiller <fucked@psychokiller.me>
2017-03-03 18:32:49 +0800

    init

---
+++ nuxt.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: '公路商店',
+    meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+    { src: '~assets/css/main.scss', lang: 'scss' },
+    'element-ui/lib/theme-default/index.css'
+  ],
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** 插件
+  */
+  plugins: ['~plugins/element-ui.js'],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLINT on save
+    */
+    vendor: ['axios'],
+    extend (config, ctx) {
+      if (ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: '/(node_modules)/'
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="container">
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <br/>
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <br/>
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <br/>
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <br/>
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <br/>
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <br/>
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <br/>
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <br/>
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <br/>
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <br/>
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <br/>
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <br/>
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <br/>
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <br/>
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <br/>
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <br/>
+    <h1 class="title">
+      Universal Vue.js Application Framework
+    </h1>
+    <button @click="open">aaa</button>
+    <nuxt-link class="button" to="/about">
+      About page
+    </nuxt-link>
+  </section>
+</template>
+<script>
+  export default {
+    methods: {
+      open () {
+        this.$message('这是一条消息提示')
+      }
+    }
+  }
+</script>
+<style scoped>
+  .title
+  {
+    margin: 50px 0;
+  }
+</style>
