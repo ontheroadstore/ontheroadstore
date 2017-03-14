a236f69c2558f3dc24a882ebc712c1ed3c84774f
PsychoKiller <fucked@psychokiller.me>
2017-03-14 18:25:58 +0800

    完成商品内容页，评论模块，点赞模块样式

--- components/store_detail/Content.vue
+++ components/store_detail/Content.vue
@@ -4,14 +4,16 @@
       <el-tabs v-model="activeName" @tab-click="handleClick">
         <el-tab-pane name="description" class="description">
           <div class="label" slot="label">描述</div>
-          <div class="content">
-            good worth一个拍官图用充气娃娃当道具的混蛋潮牌，露底裤的妞是它的经典形象。在还没烂大街之前，不管是调侃警察还是调戏大妞都游刃有余。品牌联名首选花花公子。<br><br>这款good worth烟灰缸，浮雕质感，用料厚实，合金材质拿在手里很有分量，中间是叼着烟的火烈鸟，又贱又可爱。12cm左右，不管送人还是自用都很显好。金属面有划痕为做旧效果！不是瑕疵！<br><br>拍两个包邮！私信退邮费！还有花花公子联名款(图9)159元可以点我头像购买<br><br>点击我头像，还有纹身机钥匙扣、野格酒壶、哈雷披头士杯、迷幻眼镜各种狠货
-          </div>
+          <el-row class="content">
+            <el-col :span="24">
+              good worth一个拍官图用充气娃娃当道具的混蛋潮牌，露底裤的妞是它的经典形象。在还没烂大街之前，不管是调侃警察还是调戏大妞都游刃有余。品牌联名首选花花公子。<br><br>这款good worth烟灰缸，浮雕质感，用料厚实，合金材质拿在手里很有分量，中间是叼着烟的火烈鸟，又贱又可爱。12cm左右，不管送人还是自用都很显好。金属面有划痕为做旧效果！不是瑕疵！<br><br>拍两个包邮！私信退邮费！还有花花公子联名款(图9)159元可以点我头像购买<br><br>点击我头像，还有纹身机钥匙扣、野格酒壶、哈雷披头士杯、迷幻眼镜各种狠货
+            </el-col>
+          </el-row>
         </el-tab-pane>
         <el-tab-pane name="comment" class="comment">
           <div class="badge" slot="label">评论<sup>1242</sup></div>
-          <div class="content">
-            <div class="item">
+          <el-row class="content">
+            <el-col :span="24" class="item">
               <el-row>
                 <el-col :span="4">
                   <div class="avatar">
@@ -20,34 +22,52 @@
                 </el-col>
                 <el-col :span="20">
                   <div class="nickname"><nuxt-link to="/">昵称</nuxt-link><span class="time">03/21</span></div>
-                  <div class="comment-content">回复内容</div>
-                  <div class="sub">
-
-                    <div class="item">
-                      <el-row>
-                        <el-col :span="4">
-                          <div class="avatar">
-                            <img src="https://www.etsy.com/images/avatars/default_avatar_75x75.png" />
-                          </div>
-                        </el-col>
-                        <el-col :span="20">
-                          <div class="nickname"><nuxt-link to="/">昵称</nuxt-link><span class="time">03/21</span></div>
-                          <div class="comment-content">回复内容</div>
-                        </el-col>
-                      </el-row>
+                  <div class="comment-content">
+                    回复内容
+                    <div class="sub">
+                      <div class="item">
+                        <el-row>
+                          <el-col :span="4">
+                            <div class="avatar">
+                              <img src="https://www.etsy.com/images/avatars/default_avatar_75x75.png" />
+                            </div>
+                          </el-col>
+                          <el-col :span="20">
+                            <div class="nickname"><nuxt-link to="/">昵称</nuxt-link><span class="time">03/21</span></div>
+                            <div class="comment-content">回复内容</div>
+                          </el-col>
+                        </el-row>
+                      </div>
+                      <div class="item">
+                        <el-row>
+                          <el-col :span="4">
+                            <div class="avatar">
+                              <img src="https://www.etsy.com/images/avatars/default_avatar_75x75.png" />
+                            </div>
+                          </el-col>
+                          <el-col :span="20">
+                            <div class="nickname"><nuxt-link to="/">昵称</nuxt-link><span class="time">03/21</span></div>
+                            <div class="comment-content">回复内容</div>
+                          </el-col>
+                        </el-row>
+                      </div>
                     </div>
-
                   </div>
                 </el-col>
               </el-row>
-            </div>
-          </div>
+            </el-col>
+          </el-row>
         </el-tab-pane>
         <el-tab-pane name="praise" class="praise">
           <div class="badge" slot="label">牛逼<sup>12</sup></div>
-          <div class="content">
-            asdf
-          </div>
+          <el-row class="content">
+          <el-col :span="3" class="item" v-for="item in praise_item">
+              <nuxt-link to="/">
+                <div class="item-image" v-lazy:background-image.container="item.src"></div>
+                <div class="item-nickname">昵称昵称</div>
+              </nuxt-link>
+            </el-col>
+          </el-row>
         </el-tab-pane>
       </el-tabs>
     </el-col>
@@ -57,12 +77,24 @@
   export default {
     data () {
       return {
-        activeName: 'comment'
+        activeName: 'comment',
+        praise_item: [{'url': '/', 'src': 'http://flatfull.com/themes/pulse/images/c4.jpg'},
+        {'url': '/', 'src': 'http://flatfull.com/themes/pulse/images/c1.jpg'},
+        {'url': '/', 'src': 'http://flatfull.com/themes/pulse/images/c4.jpg'},
+        {'url': '/', 'src': 'http://flatfull.com/themes/pulse/images/c1.jpg'},
+        {'url': '/', 'src': 'http://flatfull.com/themes/pulse/images/c2.jpg'},
+        {'url': '/', 'src': 'http://flatfull.com/themes/pulse/images/c3.jpg'},
+        {'url': '/', 'src': 'http://flatfull.com/themes/pulse/images/c4.jpg'},
+        {'url': '/', 'src': 'http://flatfull.com/themes/pulse/images/c1.jpg'},
+        {'url': '/', 'src': 'http://flatfull.com/themes/pulse/images/c4.jpg'},
+        {'url': '/', 'src': 'http://flatfull.com/themes/pulse/images/c1.jpg'},
+        {'url': '/', 'src': 'http://flatfull.com/themes/pulse/images/c2.jpg'},
+        {'url': '/', 'src': 'http://flatfull.com/themes/pulse/images/c3.jpg'}]
       }
     },
     methods: {
       handleClick (tab, event) {
-        console.log(tab, event)
+        // console.log(tab, event)
       }
     }
   }
@@ -98,7 +130,7 @@
       .content {
         & > .item {
           padding: 1rem 0;
-          border-bottom: 1px solid #d1dbe5;
+          border-bottom: 1px solid rgba(120, 130, 140, 0.13);
           .el-col-4 {
             text-align: center;
           }
@@ -111,8 +143,47 @@
             }
             .comment-content {
               padding: .5rem 0;
+              .sub {
+                background-color: rgba(120, 130, 140, 0.13);
+                border-radius: .2rem;
+                margin: .5rem 0;
+                .item {
+                  padding: 1rem;
+                  border-bottom: 1px solid rgba(120, 130, 140, 0.13);
+                }
+              }
             }
           }
+        }
+      }
+    }
+    .praise {
+      margin-left: -12px;
+      margin-right: -12px;
+      .item {
+        padding-left: 12px;
+        padding-right: 12px;
+        transition-property: background-color;
+        transition-duration: 0.3s;
+        transition-timing-function: ease;
+        border-radius: 5px;
+        .item-image {
+          display: block;
+          overflow: hidden;
+          border-radius: .2rem;
+          background-size: cover;
+          background-repeat: no-repeat;
+          background-position: 50% 50%;
+          &:after {
+            content: '';
+            display: block;
+            padding-top: 100%;
+          }
+        }
+        .item-nickname {
+          padding: .5rem 0;
+          text-align: center;
+          font-size: 12px;
         }
       }
     }
