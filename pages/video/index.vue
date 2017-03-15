3f0a6ad9a4c8a029b5416e1aba0ac4e7609f628c
Psychokiller <fucked@psychokiller.me>
2017-03-16 05:30:19 +0800

    添加视频列表页

---
+++ pages/video/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="bbb">aaa</div>
+    <div class="aaa"><nuxt-child /></div>
+  </div>
+</div>
+</template>
+<script>
+  export default {
+    data () {
+      return {}
+    },
+    mounted () {
+      // if (this.$route.name === 'store') {
+      //   this.$router.replace({ name: 'store-pages', params: { pages: '1' } })
+      // }
+      // console.log(this.$route.name)
+    }
+  }
+</script>
