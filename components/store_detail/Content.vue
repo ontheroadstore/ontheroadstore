2be37b60bedec147f3d4f04afe7cad71de88ee2a
Psychokiller <fucked@psychokiller.me>
2017-03-16 05:32:45 +0800

    继续完善商品内容以及商品列表页添加下拉加载功能

--- components/store_detail/Content.vue
+++ components/store_detail/Content.vue
@@ -6,7 +6,9 @@
           <div class="label" slot="label">描述</div>
           <el-row class="content">
             <el-col :span="24">
-              good worth一个拍官图用充气娃娃当道具的混蛋潮牌，露底裤的妞是它的经典形象。在还没烂大街之前，不管是调侃警察还是调戏大妞都游刃有余。品牌联名首选花花公子。<br><br>这款good worth烟灰缸，浮雕质感，用料厚实，合金材质拿在手里很有分量，中间是叼着烟的火烈鸟，又贱又可爱。12cm左右，不管送人还是自用都很显好。金属面有划痕为做旧效果！不是瑕疵！<br><br>拍两个包邮！私信退邮费！还有花花公子联名款(图9)159元可以点我头像购买<br><br>点击我头像，还有纹身机钥匙扣、野格酒壶、哈雷披头士杯、迷幻眼镜各种狠货
+              <article>
+                good worth一个拍官图用充气娃娃当道具的混蛋潮牌，露底裤的妞是它的经典形象。在还没烂大街之前，不管是调侃警察还是调戏大妞都游刃有余。品牌联名首选花花公子。<br><br>这款good worth烟灰缸，浮雕质感，用料厚实，合金材质拿在手里很有分量，中间是叼着烟的火烈鸟，又贱又可爱。12cm左右，不管送人还是自用都很显好。金属面有划痕为做旧效果！不是瑕疵！<br><br>拍两个包邮！私信退邮费！还有花花公子联名款(图9)159元可以点我头像购买<br><br>点击我头像，还有纹身机钥匙扣、野格酒壶、哈雷披头士杯、迷幻眼镜各种狠货
+              </article>
             </el-col>
           </el-row>
         </el-tab-pane>
@@ -61,7 +63,7 @@
         <el-tab-pane name="praise" class="praise">
           <div class="badge" slot="label">牛逼<sup>12</sup></div>
           <el-row class="content">
-          <el-col :span="3" class="item" v-for="item in praise_item">
+            <el-col :span="3" class="item" v-for="item in praise_item">
               <nuxt-link to="/">
                 <div class="item-image" v-lazy:background-image.container="item.src"></div>
                 <div class="item-nickname">昵称昵称</div>
