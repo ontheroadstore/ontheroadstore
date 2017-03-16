5a2a309438165d024fe5c888b40f21d5a0d314f8
Psychokiller <fucked@psychokiller.me>
2017-03-17 02:11:27 +0800

    element 1.2.4 <el-col>模块有个小问题，span = 0没有反应，等待官方修复

--- pages/video/detail/_id.vue
+++ pages/video/detail/_id.vue
@@ -19,8 +19,23 @@
       </article>
     </el-col>
     <el-col class="recommend" :span="24">
-      推荐视频
-
+      <el-row>
+        <el-col :xs="{ span: '0' }" :sm="{ span: '0' }" :md="4" :lg="4" class="title">推荐视频</el-col>
+        <el-col :xs="12" :sm="12" :md="5" :lg="5" v-for="item in banner" class="item">
+          <nuxt-link :to="{ name: 'video-detail-id', params: { id: '123' } }">
+            <div class="item-image" v-lazy:background-image.container="item.src">
+              <span class="item-tag">混蛋出差</span>
+            </div>
+            <div class="item-title">
+              <h3>这是一个标题这是一个标题这是一个标题这是一个标题这是一个标题这是一个标题</h3>
+              <p>
+                <span><i class="el-icon-time"></i>2017/02/02</span>
+                <span><i class="el-icon-view"></i>3k</span>
+              </p>
+            </div>
+          </nuxt-link>
+        </el-col>
+      </el-row>
     </el-col>
   </el-row>
 </div>
@@ -95,6 +110,7 @@
         }
       }
       .title {
+
         h1 {
           margin: 0;
         }
@@ -111,6 +127,87 @@
       }
       .recommend {
         background-color: #1c202b;
+        padding: 1.5rem 2rem;
+        .el-row {
+          margin: 0 -12px;
+          .el-col {
+            color: #fff;
+            &.item {
+              padding-left: 12px;
+              padding-right: 12px;
+              // padding-bottom: 6px;
+              a {
+                display: block;
+                &:hover {
+                  .item-image {
+                    .item-tag {
+                      background-color: #6cc788;
+                      transition-property: background-color;
+                      transition-duration: 0.3s;
+                      transition-timing-function: ease;
+                    }
+                    &:after {
+                      transition-property: background-color;
+                      transition-duration: 0.3s;
+                      transition-timing-function: ease;
+                      background-color: rgba(0, 0, 0, 0.2);
+                    }
+                  }
+                  .item-title {
+                    opacity: 1;
+                  }
+                }
+                .item-image {
+                  border-radius: inherit;
+                  background-size: cover;
+                  background-repeat: no-repeat;
+                  background-position: 50% 50%;
+                  background-color: rgba(120,120,120,.2);
+                  border-radius: 0.2rem;
+                  position: relative;
+                  .item-tag {
+                    position: absolute;
+                    bottom: .5rem;
+                    right: .5rem;
+                    padding: .4rem;
+                    line-height: 1em;
+                    height: 1em;
+                    background-color: #767a7e;
+                    border-radius: 0.2rem;
+                    color: #fff;
+                    font-size:12px;
+                  }
+                  &:after {
+                    content: '';
+                    display: block;
+                    padding-bottom: 56.25%;
+                  }
+                }
+                .item-title {
+                  display: block;
+                  white-space: nowrap;
+                  overflow: hidden;
+                  text-overflow: ellipsis;
+                  opacity: .9;
+                  h3 {
+                    margin: .5em 0;
+                  }
+                  p {
+                    margin: 0;
+                    // margin-bottom: 1em;
+                    opacity: .6;
+                    span {
+                      margin-right: 1rem;
+                      i {
+                        margin-right: .5rem;
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
       }
     }
   }
