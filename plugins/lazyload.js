f82177acaeec6a9c433c48d7fd422e605dc909f7
PsychoKiller <fucked@psychokiller.me>
2017-04-18 21:25:28 +0800

    调整插件以及nuxt配置

--- plugins/lazyload.js
+++ plugins/lazyload.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueLazyload from 'vue-lazyload'
-if (process.BROWSER_BUILD) {
-  Vue.use(VueLazyload, {
-    attempt: 0
-  })
-}
+Vue.use(VueLazyload, {
+  attempt: 0
+})
+
