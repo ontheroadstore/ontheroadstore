52ecd23134ca7f45138f79a7cb96ea40a13d4974
PsychoKiller <fucked@psychokiller.me>
2017-04-18 20:50:36 +0800

    调整api文档，测试github提交

--- api/index.js
+++ api/index.js
@@ -39,5 +39,40 @@ export default {
       pagesize: 20,
       page: page
     })
+  },
+  getStoreDetail: (id) => {
+    return fetchApi('store/detail', {
+      id: id
+    })
+  },
+  getStoreDetailSellers: (id) => {
+    return fetchApi('store/detail_sellers', {
+      id: id
+    })
+  },
+  getStoreDetailInfo: (id) => {
+    return fetchApi('store/detail_info', {
+      id: id
+    })
+  },
+  getArticleIndexList: (page) => {
+    return fetchApi('article/list', {
+      pagesize: 20,
+      page: page
+    })
+  },
+  getArticleDetail: (id) => {
+    return fetchApi('article/detail', {
+      id: id
+    })
+  },
+  getArticleMore: (page) => {
+    return fetchApi('article/list', {
+      pagesize: 20,
+      page: page
+    })
+  },
+  getArticleRecommend: () => {
+    return fetchApi('article/recommend')
   }
 }
